c68d77a370d482e94440bd0c9e2dd92d49888190
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-08-19 12:05:23 -0400

    imported useState for navbarOpen function

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import NavLink from './NavLink';
 
@@ -18,6 +18,9 @@ const NavLinks = [
     }
 ]
 const Navbar = () => {
+
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
     return (
         <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
             <div className="flex flex-wrap items-center  justify-between mx-auto px-4">
