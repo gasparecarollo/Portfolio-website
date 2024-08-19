14bdaf913a11a4f6baa1fcbae6a316ead1d9ecf5
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-08-19 12:02:39 -0400

    Installed heroicons

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import HeroSection from "./Components/HeroSection";
-
+import Navbar from "./Components/Navbar"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div class="container mx-auto px-12 py-4">
+      <Navbar />
+      <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
       </div>
     </main>
