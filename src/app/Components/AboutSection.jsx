0855663d2b933004eaf71cd7ee9b6fc20764ce44
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-08-24 17:50:01 -0400

    Added TabButtons for skills, certificates and education

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -60,20 +60,30 @@ const AboutSection = () => {
             <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 className='text-4xl font-bold text-white mb-4'> About Me</h2>
                 <p className='text-base lg:text-lg'> I am a full stack web devoloper.</p>
-                <div className='flex flex-row mt-8' >
+                <div className='flex flex-row justify-start mt-8' >
                     <TabButton selectTab={() =>
                         handleTabChange("skills")} active={tab === "skills"}
-                    > Skills </TabButton>
-                    <TabButton selectTab={tab === "certifications"} >
+                    >
                         {" "}
-                        Certifications {" "}
+                        Skills{" "}
+                    </TabButton>
+                    <TabButton
+                        selectTab={() => handleTabChange("education")} active={tab === "education"} >
+                        {" "}
+                        Education {" "}
+                    </TabButton>
+                    <TabButton
+                        selectTab={() => handleTabChange("certifications")}
+                        active={tab === "certifications"}
+                    >
+                        {" "}
+                        Certifications{" "}
                     </TabButton>
                 </div>
                 <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}
                 </div>
             </div>
         </div>
-
         </section>
     );
 };
